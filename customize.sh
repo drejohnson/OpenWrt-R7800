5a0007c0c6cd53693c77b6898f65320918c5a805
DeAndre Johnson <dre@phreshr.com>
2022-02-28 21:07:09 +0800

    update

--- customize.sh
+++ customize.sh
@@ -1,4 +1,12 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+# Add UPX UCL Toolkit
+mkdir -p tools/ucl && wget -P tools/ucl https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/ucl/Makefile
+mkdir -p tools/upx && wget -P tools/upx https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/upx/Makefile
+
+# Modify MakeFile
+sed  -i '/^# builddir dependencies/i\tools-y += ucl upx' ./tools/Makefile
+sed  -i '/^# builddir dependencies/a\$(curdir)/upx/compile := $(curdir)/ucl/compile' ./tools/Makefile
+
 
